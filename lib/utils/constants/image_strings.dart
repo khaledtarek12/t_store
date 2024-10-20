bf0dec2e0231911e2db9610c83aac874c310c15c
khaledtarek12 <tareqkhalid092@gmail.com>
2024-10-21 01:07:07 +0300

    ..

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -10,6 +10,15 @@ class TImages {
   static const String onboardingImage3 =
       'assets/images/on_boarding_images/onboarding3.gif';
 
+  static const String checkEmaiLight =
+      'assets/images/on_boarding_images/check_emai_light.gif';
+  static const String checkEmaiDark =
+      'assets/images/on_boarding_images/chek_email_dark.gif';
+  static const String forgetPassword =
+      'assets/images/on_boarding_images/forgotPassword.gif';
+  static const String resetPassword =
+      'assets/images/on_boarding_images/reset-password-compressed.gif';
+
   // Social logo
   static const String google = 'assets/logos/google.png';
   static const String facebook = 'assets/logos/facebook.png';
