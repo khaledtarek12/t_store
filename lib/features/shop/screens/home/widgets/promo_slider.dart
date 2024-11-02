d73bc2b5da9fa11f87b5f92a7b4b7535c8c4fe9e
khaledtarek12 <tareqkhalid092@gmail.com>
2024-11-02 02:40:29 +0200

    page_view_dot_indicator

--- lib/features/shop/screens/home/widgets/promo_slider.dart
+++ lib/features/shop/screens/home/widgets/promo_slider.dart
@@ -1,7 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:t_store/common/widgets/custom_shapes/container/circular_container.dart';
+import 'package:page_view_dot_indicator/page_view_dot_indicator.dart';
 import 'package:t_store/common/widgets/images/t_rounded_images.dart';
 import 'package:t_store/features/shop/controllers/home_controller.dart';
 import 'package:t_store/utils/constants/colors.dart';
@@ -14,38 +14,32 @@ class TpromoSlider extends StatelessWidget {
   });
 
   final List banners;
-
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(HomeController());
     return Column(
       children: [
         CarouselSlider(
-            options: CarouselOptions(
-              viewportFraction: 1,
-              onPageChanged: (index, reason) {
-                controller.updatePageIndicator(index);
-              },
-            ),
-            items:
-                banners.map((url) => TRoundedlmages(imageUrl: url)).toList()),
+          options: CarouselOptions(
+            viewportFraction: 1,
+            onPageChanged: (index, reason) {
+              controller.updatePageIndicator(index);
+            },
+          ),
+          items: banners.map((url) => TRoundedlmages(imageUrl: url)).toList(),
+        ),
         const SizedBox(height: TSizes.spaceBtwItems),
         Obx(() {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              for (int i = 0; i < banners.length; i++)
-                TCircularContainer(
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.decelerate,
-                  margin: const EdgeInsets.only(right: 10),
-                  width: controller.carsoualCurrentIndex.value == i ? 30 : 15,
-                  height: 5,
-                  backgroundCoIor: controller.carsoualCurrentIndex.value == i
-                      ? TColors.primary
-                      : TColors.grey,
-                ),
-            ],
+          return PageViewDotIndicator(
+            currentItem: controller.carsoualCurrentIndex.value,
+            count: banners.length,
+            unselectedColor: TColors.grey,
+            selectedColor: TColors.primary,
+            duration: const Duration(milliseconds: 300),
+            boxShape: BoxShape.circle,
+            size: const Size(20, 15),
+            unselectedSize: const Size(10, 5),
+            margin: const EdgeInsets.symmetric(horizontal: 5),
           );
         }),
       ],
